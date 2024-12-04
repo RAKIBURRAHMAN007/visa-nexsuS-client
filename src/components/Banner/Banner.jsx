5a23421a5cd41b98f18ae89c2f9e608ba648fbac
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2024-12-04 16:32:42 +0600

    banner updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,10 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-import img1 from "../../assets/banner1.png"
-import img2 from "../../assets/banner2.jpg"
-import img3 from "../../assets/banner3.jpg"
-import img4 from "../../assets/banner4.jpg"
-// Import Swiper styles
 import 'swiper/css';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import img2 from "../../assets/banner2.jpg";
+import img3 from "../../assets/banner3.jpg";
+import img4 from "../../assets/banner4.jpg";
 
 const Banner = () => {
     return (
@@ -16,7 +13,7 @@ const Banner = () => {
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 2000,
+                    delay: 3000,
                     disableOnInteraction: false,
                 }}
                 pagination={{
@@ -26,15 +23,41 @@ const Banner = () => {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                
-                <SwiperSlide><img className='w-full bg-cover md:h-[500px]' src={img1} alt="" /></SwiperSlide>
-                <SwiperSlide><img className='w-full bg-cover md:h-[500px]' src={img2} alt="" /></SwiperSlide>
-                <SwiperSlide><img className='w-full bg-cover md:h-[500px]' src={img3} alt="" /></SwiperSlide>
-                <SwiperSlide><img className='w-full bg-cover md:h-[500px]' src={img4} alt="" /></SwiperSlide>
-                
+                {/* Slide 1 */}
+                <SwiperSlide>
+                    <div className="relative w-full">
+                        <img className='w-full bg-cover md:h-[500px]' src={img2} alt="Banner 2" />
+                        <div className="absolute bottom-4 mx-3  md:bottom-10 md:left-5 text-white bg-black/80 md:p-4 p-1 rounded-lg">
+                            <h2 className="text-xs md:text-4xl font-bold text-shadow-md">Study Abroad Opportunities</h2>
+                            <p className="text-xs md:text-lg mt-2">Explore educational visas for top international institutions.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
 
+                {/* Slide 2 */}
+                <SwiperSlide>
+                    <div className="relative w-full">
+                        <img className='w-full bg-cover h-[200px] md:h-[500px]' src={img3} alt="Banner 3" />
+                        <div className="absolute bottom-4 mx-3  md:bottom-10 md:left-5 text-white bg-black/80 md:p-4 p-1 rounded-lg">
+                            <h2 className="text-xs md:text-4xl font-bold text-shadow-md">Work and Employment Visas</h2>
+                            <p className=" text-xs md:text-lg mt-2">Find the right visa for your global career goals.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                {/* Slide 3 */}
+                <SwiperSlide>
+                    <div className="relative w-full">
+                        <img className='w-full bg-cover h-[200px] md:h-[500px]' src={img4} alt="Banner 4" />
+                        <div className="absolute bottom-4 mx-3  md:bottom-10 md:left-5 text-white bg-black/80 md:p-4 p-1 rounded-lg">
+                            <h2 className="text-xs md:text-4xl font-bold text-shadow-md">Tourist Visas Made Simple</h2>
+                            <p className=" text-xs md:text-lg mt-2">Plan your dream vacation with hassle-free visa services.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </div>
     );
 };
-export default Banner
+
+export default Banner;
