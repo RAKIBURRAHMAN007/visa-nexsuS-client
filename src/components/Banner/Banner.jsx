37bd088b2df720ecc0b1a85d085dd829f9ae5e80
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-01-06 21:45:42 +0600

    banner section updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ import img4 from "../../assets/banner4.jpg";
 
 const Banner = () => {
     return (
-        <div className='w-11/12 mx-auto mt-2'>
+        <div className=' mt-2'>
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -26,7 +26,7 @@ const Banner = () => {
                 {/* Slide 1 */}
                 <SwiperSlide>
                     <div className="relative w-full">
-                        <img className='w-full bg-cover h-[200px]  md:h-[500px]' src={img2} alt="Banner 2" />
+                        <img className='w-full object-cover h-[200px]  md:h-[70vh]' src={img2} alt="Banner 2" />
                         <div className="absolute bottom-4 mx-3  md:bottom-10 md:left-5 text-white bg-black/80 md:p-4 p-1 rounded-lg">
                             <h2 className="text-xs md:text-4xl font-bold text-shadow-md">Study Abroad Opportunities</h2>
                             <p className="text-xs md:text-lg mt-2">Explore educational visas for top international institutions.</p>
@@ -36,7 +36,7 @@ const Banner = () => {
 
                 <SwiperSlide>
                     <div className="relative w-full">
-                        <img className='w-full bg-cover h-[200px] md:h-[500px]' src={img4} alt="Banner 4" />
+                        <img className='w-full   h-[200px] md:h-[70vh]' src={img4} alt="Banner 4" />
                         <div className="absolute bottom-4 mx-3  md:bottom-10 md:left-5 text-white bg-black/80 md:p-4 p-1 rounded-lg">
                             <h2 className="text-xs md:text-4xl font-bold text-shadow-md">Tourist Visas Made Simple</h2>
                             <p className=" text-xs md:text-lg mt-2">Plan your dream vacation with hassle-free visa services.</p>
@@ -46,7 +46,7 @@ const Banner = () => {
                 
                 <SwiperSlide>
                     <div className="relative w-full">
-                        <img className='w-full bg-cover h-[200px] md:h-[500px]' src={img3} alt="Banner 3" />
+                        <img className='w-full object-cover h-[200px] md:h-[70vh]' src={img3} alt="Banner 3" />
                         <div className="absolute bottom-4 mx-3  md:bottom-10 md:left-5 text-white bg-black/80 md:p-4 p-1 rounded-lg">
                             <h2 className="text-xs md:text-4xl font-bold text-shadow-md">Work and Employment Visas</h2>
                             <p className=" text-xs md:text-lg mt-2">Find the right visa for your global career goals.</p>
