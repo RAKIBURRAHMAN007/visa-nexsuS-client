b56dbb31a1f82f6db1c7f10bed8edc04e1183359
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-01-06 21:50:39 +0600

    country tooltip section updated

--- src/components/Countrys/Countrys.jsx
+++ src/components/Countrys/Countrys.jsx
@@ -4,7 +4,7 @@ import CountryToolTip from './CountryToolTip';
 
 const Countrys = () => {
     return (
-        <div className='mt-20'>
+        <div className='mt-20 w-11/12 mx-auto'>
             <h1 className='font-bold text-xl text-center md:text-2xl'>Countries we’re supporting <br />
             for the <span className='text-red-500'>immigration</span></h1>
 
